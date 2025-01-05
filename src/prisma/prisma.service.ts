65703bb670142c301ad4583420b7bacd4851e745
Gabriel <gabriel.g2d2@gmail.com>
2025-01-04 21:08:21 -0300

    refactor: extend AdapterPrismaService from PrismaClient for improved database access

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,16 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class AdapterPrismaService {
+export class AdapterPrismaService extends PrismaClient {
   public client: PrismaClient;
 
-  // constructor(private readonly _prisma = new PrismaClient()) {}
-
-  // get prisma() {
-  //   return this._prisma;
-  // }
-
   constructor() {
-    this.client = new PrismaClient();
+    super();
   }
 }
