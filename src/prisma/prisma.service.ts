2b9b6e14ca4f2fef673022fc96ee48e2f2b7f0ca
Gabriel <gabriel.g2d2@gmail.com>
2025-01-04 21:16:02 -0300

    feat: implement lifecycle hooks for database connection management in AdapterPrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class AdapterPrismaService extends PrismaClient {
+export class AdapterPrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   public client: PrismaClient;
 
   constructor() {
     super({
       log: ['warn', 'error'],
     });
+  }
+
+  onModuleInit() {
+    // TODO: Add a logger here
+    console.log('Connecting to the database');
+    return this.$connect();
+  }
+
+  onModuleDestroy() {
+    // TODO: Add a logger here
+    console.log('Disconnecting from the database');
+    return this.$disconnect();
   }
 }
